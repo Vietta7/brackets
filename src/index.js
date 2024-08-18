31d8298ac8ba929bdee1700dc821b73b71bec10d
Anna <annaspacer7@gmail.com>
2024-08-18 19:18:00 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let configMap = new Map(bracketsConfig); // Создала карту для быстрого поиска пар
+
+  for (let char of str) {
+      if (configMap.has(char)) { // Если символ является открывающей скобкой или спец случаем (||)
+          if (stack.length > 0 && stack[stack.length - 1] === char && configMap.get(char) === char) {
+              stack.pop(); // Закрывающая скобка в спец случае
+          } else {
+              stack.push(char); // Открывающая скобка
+          }
+      } else {
+          if (stack.length === 0 || configMap.get(stack.pop()) !== char) {
+              return false; // Скобки не совпадают
+          }
+      }
+  }
+
+  return stack.length === 0; // Если стек пуст, все скобки правильно закрыты
+};
